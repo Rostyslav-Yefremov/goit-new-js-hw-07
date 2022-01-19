07103049b0c32eec0646b0834d0a97dc87489443
Rostyslav-Yefremov <rostislavefremow@gmail.com>
2022-01-19 17:59:35 +0200

    Make gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map((elem) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${elem.original}">
+    <img
+      class="gallery__image"
+      src="${elem.preview}"
+      data-source="${elem.original}"
+      alt="${elem.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+galleryRef.addEventListener('click', (event) => {
+  event.preventDefault();
+  const urlBigImg = event.target.dataset.source;
+  console.log(urlBigImg);
+});
